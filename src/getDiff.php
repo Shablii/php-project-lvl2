7150377336d0c53d8e576628366539492ffff1c4
Vadim Shablii <mefistovad@gmail.com>
2021-05-17 18:55:48 +0300

    add src/getDiff.php

---
+++ src/getDiff.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Differ\Gendiff;
+
+function getDiff($file1, $file2): string
+{
+    $result = [];
+    $flow1 = json_decode(file_get_contents($file1), true);
+    $flow2 = json_decode(file_get_contents($file2), true);
+
+    $resultFlow = collect(array_merge($flow1, $flow2))->sortKeys();
+
+    $result = $resultFlow->map(function ($item, $key) use ($flow1, $flow2) {
+        if (array_key_exists($key, $flow2) && array_key_exists($key, $flow1)) {
+            if ($flow1[$key] === $flow2[$key]) {
+                $item = pars($item);
+                return "  $key: $item";
+            } else {
+                return "- $key: $flow1[$key]\n+ $key: $item";
+            }
+        } elseif (array_key_exists($key, $flow2) && !array_key_exists($key, $flow1)) {
+            $item = pars($item);
+            return "+ $key: $item";
+        } elseif (!array_key_exists($key, $flow2) && array_key_exists($key, $flow1)) {
+            $item = pars($item);
+            return  "- $key: $item";
+        }
+    })->all();
+
+    return implode("\n", $result);
+}
+
+function pars($str): string
+{
+    if (is_bool($str)) {
+        $str = $str = (true === $str) ? "true" : "false";
+    }
+    return $str;
+}
