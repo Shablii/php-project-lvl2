779ea2fb9eba637394787bc10beffc47769b5537
Vadim Shablii <mefistovad@gmail.com>
2021-07-16 22:59:03 +0300

    auto test part3

--- src/Formatters/Json.php
+++ src/Formatters/Json.php
@@ -2,11 +2,11 @@
 
 namespace Differ\Formatters\Json;
 
-function json(array $ast): string | array
+function json(array $ast): string
 {
     $json = json_encode($ast);
     if ($json === false) {
-        throw new \Exception("Can't convert to json: $ast");
+        throw new \Exception("Can't convert to json");
     }
     return $json;
 }
